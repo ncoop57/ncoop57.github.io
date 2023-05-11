301da59a41e9c85c4db363f682e4450c0102575b
ncoop57 <nacooper01@email.wm.edu>
2023-05-11 09:38:59 -0400

    Update about page to have new info on job and graduation

--- src/components/about.js
+++ src/components/about.js
@@ -38,16 +38,20 @@ const About = props => {
         <Divider />
         <br />
         <Typography align="justify" component="p">
-          Hi there, my name is Nathan Cooper. I am currently a Ph.D. student at
-          the College of William and Mary under the mentorship of Dr.
+          Hi there, my name is Nathan Cooper. I am currently the Lead Research Scientist for the <a href="http://www.carper.ai/codeai">Code.AI</a> Team at <a href="http://www.stability.ai/">Stability.AI</a>.
+          My work focuses on helping software developers by applying Deep Learning to Software Engineering problems such as code generation and bug fixing.
+          I received my Ph.D. from the College of William and Mary in Computer Science in 2023 under the awesome advisement of Dr.
           <a href="http://www.cs.wm.edu/~denys/"> Denys Poshyvanyk</a>.
-          My research interests are at the intersection of Software engineering
-          and Machine Learning. I hope to contribute to educating the world by helping creating the next
+          I hope to eventually contribute to educating the world by helping creating the next
           generation of education tools such as the{" "}
           <a href="https://www.khanacademy.org/">Khan Academy</a> and the{" "}
-          <a href="https://www.edx.org/">edX</a> platform that democratizes knowledge and education.
-          It is my belief that personalized learning will be the future of education
+          <a href="https://www.edx.org/">edX</a> platform that democratizes knowledge and education. It is my belief that personalized learning will be the future of education
           and I hope to contribute to that vision.
+          I also hope to some of the most innocent, namely children who are subjected to
+          some of the most ugly and cruel parts of the world, by helping victims of human trafficking,
+          CSAM, and other forms of abuse with companies such as{" "}
+          <a href="https://www.thorn.org/our-work-to-stop-child-sexual-exploitation/">Thorn</a>.
+
 
           <br />
           <br />
@@ -56,7 +60,7 @@ const About = props => {
           find a list of, hopefully mostly complete, ones on my Projects page.
           Below you will find my resume if you are curious in any of my other
           experiences or accomplishments. If you have any questions or just want
-          to say hi, feel free to connect via email at <a href="mailto:nacooper01@email.wm.edu">nacooper01@email.wm.edu</a>,
+          to say hi, feel free to connect via email at <a href="mailto:nathan.cooper@stability.ai">nathan.cooper@stability.ai</a>,
           Twitter <a href="https://twitter.com/ncooper57">@ncooper57</a>, or <a href="https://www.linkedin.com/in/nathan-cooper-820292106/">LinkedIn</a>!
         </Typography>
 
